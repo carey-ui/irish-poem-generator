4c7a52af868dc0defc20c4f2b9ae7dc704208c37
carey-ui <careyf30@gmail.com>
2026-03-08 02:00:40 +0000

    added Javascript

--- SRC/index.js
+++ SRC/index.js
@@ -1,1 +1,13 @@
+function generatePoem(event) {
+  event.preventDefault();
 
+  new Typewriter("#poem", {
+    strings: ["small spaces of silence"],
+    autoStart: true,
+    delay: 30,
+    cursor: "",
+  });
+}
+
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
