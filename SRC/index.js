8498bee2f11dd450e253549b2841ddcc4b4ff2e5
carey-ui <careyf30@gmail.com>
2026-03-08 16:58:38 +0000

    Improved User Experience
    Added Hints to HTML, added Loader animation

--- SRC/index.js
+++ SRC/index.js
@@ -1,4 +1,10 @@
 function displayPoem(response) {
+  let loaderElement = document.querySelector("#loader");
+  loaderElement.classList.add("hidden");
+
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.add("fade-in");
+
   new Typewriter("#poem", {
     strings: response.data.answer,
     autoStart: true,
@@ -16,6 +22,12 @@ function generatePoem(event) {
   let prompt = `create a short poem in Irish about ${instructionsInput}`;
   let context = `be romantic and whismical, just generate a short 4 line poem based on ${instructionsInput} using this example: <p>this is a poem,<br/>this is a poem</p>`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let poemElement = document.querySelector("#poem");
+  let loaderElement = document.querySelector("#loader");
+  loaderElement.classList.remove("hidden");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `Generating a poem about ${instructionsInput} in Irish`;
 
   console.log("Generating Poem");
   console.log(`prompt: ${prompt}`);
