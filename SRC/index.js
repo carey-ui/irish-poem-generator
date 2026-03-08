60ffc250c307528b25a98dc71b2706883c3a05b3
carey-ui <careyf30@gmail.com>
2026-03-08 14:47:46 +0000

    Added AI API integration

--- SRC/index.js
+++ SRC/index.js
@@ -1,12 +1,27 @@
-function generatePoem(event) {
-  event.preventDefault();
-
+function displayPoem(response) {
   new Typewriter("#poem", {
-    strings: ["small spaces of silence"],
+    strings: response.data.answer,
     autoStart: true,
     delay: 30,
     cursor: "",
   });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions").value;
+
+  let apiKey = "acbod87f7ctb68340e47b7b3abe9ae79";
+  let prompt = `create a short poem in Irish about ${instructionsInput}`;
+  let context = `be romantic and whismical, just generate a short 4 line poem based on ${instructionsInput} using this example: <p>this is a poem,<br/>this is a poem</p>`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log("Generating Poem");
+  console.log(`prompt: ${prompt}`);
+  console.log(`context: ${context}`);
+
+  axios.get(apiUrl).then(displayPoem);
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
